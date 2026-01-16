98b701da540b2b72242e147df3cd7ab874408c9d
Oscar <oscarodriguez56@gmail.com>
2026-01-16 09:30:12 +0100

    Añadiendo informacion relevante

--- src/main/java/org/example/Secuencia.java
+++ src/main/java/org/example/Secuencia.java
@@ -4,17 +4,13 @@ import org.example.controller.MetodosPresidente;
 import org.example.controller.PaisController;
 import org.example.model.Pais;
 import org.example.model.Presidente;
-import org.example.model.TipoPais;
-import org.example.services.PaisService;
-import org.example.services.PresidenteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
 @Service
 public class Secuencia {
     MetodosPresidente metodosPresidente;
@@ -25,23 +21,14 @@ public class Secuencia {
         this.paisController = paisController;
     }
     public void ejecutarSecuencia() {
-        Presidente presidente = metodosPresidente.anadirPresidente(new Presidente(12,"JEKL","OSLE"));
+        Presidente[] presidentes = importarDesdeJSONPresidente("presidentes.json");
+        for (Presidente presidente : presidentes) {
+            metodosPresidente.anadirPresidente(presidente);
+        }
         Pais[] paises = importarDesdeJSONPais("paises.json");
         for (Pais pais : paises) {
             paisController.anadirPais(pais);
         }
-        metodosPresidente.obtenerPresidente(presidente.getId()).ifPresent(System.out::println);
-        presidente.setEdade(18);
-        metodosPresidente.anadirPresidente(presidente);
-        Pais pais = paisController.anadirPais(new Pais("España", TipoPais.monarquiaParlamentaria,presidente.getId(), (java.util.ArrayList<String>) List.of("MEL","lsd","JKEL"))).getBody();
-        if (pais!=null) pais.setOrganizacion(TipoPais.democracia);
-        paisController.anadirPais(pais);
-        if (pais!=null) {
-            Pais p = paisController.obtenerPais(pais.getId()).getBody();
-            if (p!=null) System.out.println(p);
-            paisController.eliminarPais(pais);
-        }
-        metodosPresidente.eliminarPresidente(presidente);
 
     }
     public Pais[] importarDesdeJSONPais(String archivoJSON) {
@@ -51,6 +38,15 @@ public class Secuencia {
         } catch (IOException e) {
             System.err.println("Error al importar el JSON: " + e.getMessage());
             return new Pais[0];
+        }
+    }
+    public Presidente[] importarDesdeJSONPresidente(String archivoJSON) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readValue(new File(archivoJSON), Presidente[].class);
+        } catch (IOException e) {
+            System.err.println("Error al importar el JSON: " + e.getMessage());
+            return new Presidente[0];
         }
     }
 }
